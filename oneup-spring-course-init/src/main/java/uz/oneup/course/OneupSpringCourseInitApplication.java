6764461cad3acdeda02288127ae90e91fbe9aa33
Xushnudbek Babayev <arteldeveloper@gmail.com>
2021-12-22 19:49:31 +0500

    oneup-spring-course-init

--- oneup-spring-course-init/src/main/java/uz/oneup/course/OneupSpringCourseInitApplication.java
+++ oneup-spring-course-init/src/main/java/uz/oneup/course/OneupSpringCourseInitApplication.java
@@ -1,13 +1,34 @@
 package uz.oneup.course;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class OneupSpringCourseInitApplication {
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${server.port}")
+    private String applicationPort;
+
     public static void main(String[] args) {
         SpringApplication.run(OneupSpringCourseInitApplication.class, args);
+    }
+
+    @GetMapping
+    public ResponseEntity<String> init() {
+
+        return new ResponseEntity<>(
+                applicationName+ " is Running on Port "+ applicationPort,
+                HttpStatus.OK
+        );
     }
 
 }
