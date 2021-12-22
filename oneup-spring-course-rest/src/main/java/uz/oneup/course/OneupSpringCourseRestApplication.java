f6996a77139f94a4be10882efc20583b98b2fe54
Xushnudbek Babayev <arteldeveloper@gmail.com>
2021-12-22 21:31:03 +0500

    oneup-spring-course-rest

--- oneup-spring-course-rest/src/main/java/uz/oneup/course/OneupSpringCourseRestApplication.java
+++ oneup-spring-course-rest/src/main/java/uz/oneup/course/OneupSpringCourseRestApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class OneupSpringCourseRestApplication {
 
