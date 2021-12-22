f570ccfec543187e517dd8a69a59c087fd61caad
Xushnudbek Babayev <arteldeveloper@gmail.com>
2021-12-22 17:26:33 +0500

    oneup-spring-course-setup

---
+++ oneup-spring-course-setup/src/main/java/uz/oneup/course/OneupSpringCourseSetupApplication.java
@@ -0,0 +1,13 @@
+package uz.oneup.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneupSpringCourseSetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneupSpringCourseSetupApplication.class, args);
+    }
+
+}
